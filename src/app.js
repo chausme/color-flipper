587284b9055726d42f18ab809ee2d8100dc704a6
Alex Chaus <chausme@gmail.com>
2022-12-04 16:14:16 +0400

    Add ESLint and fix files

--- src/app.js
+++ src/app.js
@@ -2,13 +2,11 @@ const colors = ['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', function () {
+const getRandomNumber = () => Math.floor(Math.random() * colors.length);
+
+btn.addEventListener('click', () => {
     // Get random number between 0 and color array length
     const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
-
-const getRandomNumber = () => {
-    return Math.floor(Math.random() * colors.length);
-};
